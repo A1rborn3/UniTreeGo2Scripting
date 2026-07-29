a6caae63f8b52e015b8c9016760f4645b9ec8861
A1rborn3 <ethan.young.nz@gmail.com>
2026-07-29 18:00:31 +1200

    Tested working script for movement and handshakes with emergency overides

--- unitree_sdk2_python/SmartParkingTesting/Go2KeyboardController.py
+++ unitree_sdk2_python/SmartParkingTesting/Go2KeyboardController.py
@@ -5,46 +5,57 @@ from unitree_sdk2py.core.channel import ChannelFactoryInitialize
 from unitree_sdk2py.go2.sport.sport_client import SportClient
 from unitree_sdk2py.go2.obstacles_avoid.obstacles_avoid_client import ObstaclesAvoidClient
 
-class Go2KeyboardController(SportClient, ObstaclesAvoidClient):
-    def __init__(self, network_interface: str = "lo"):#change to 'lo'/'eth0' for local or connected
-        SportClient.__init__(self)
-        ObstaclesAvoidClient.__init__(self) #setup sport client and obi avoid
 
-        self.SetTimeout(10.0)
-        SportClient.Init(self)  # Initialize sport client
-        ObstaclesAvoidClient.Init(self)# Initialize obstacle avoidance client
+class Go2KeyboardController:
+    def __init__(self, network_interface: str = "eth0"):#change to 'lo'/'eth0' for local or connected
+        self.sport_client = SportClient()
+        self.obstacle_client = ObstaclesAvoidClient()
+ #setup sport client and obi avoid
+
+        self.sport_client.SetTimeout(10.0)
+        self.sport_client.Init()
+        self.obstacle_client.Init()
 
         self.is_standing = True #may not be true on startup
         self.pressed_keys = set()
         self.obstacle_avoidance_enabled = False
 
     def enable_obstacle_avoidance(self):
-        self.UseRemoteCommandFromApi(True)  # Tell system to accept API commands
-        self.SwitchSet(True)  # Enable obstacle avoidance
+        self.obstacle_client.UseRemoteCommandFromApi(True)
+        self.obstacle_client.SwitchSet(True)  # Enable obstacle avoidance
         self.obstacle_avoidance_enabled = True
         time.sleep(0.5)
 
     def disable_obstacle_avoidance(self):
-        self.SwitchSet(False)
-        self.UseRemoteCommandFromApi(False)
+        self.obstacle_client.SwitchSet(False)
+        self.obstacle_client.UseRemoteCommandFromApi(False)
         self.obstacle_avoidance_enabled = False
 
+    def wave(self):
+        self.pressed_keys.clear()
+        self.sport_client.StopMove()
+        time.sleep(0.5)
+        self.sport_client.Hello()
 
     def stand_up(self):
-        self.StopMove()
+        
+        self.sport_client.StopMove()
         time.sleep(0.2)
-        self.StandUp()
+        self.sport_client.StandUp()
         self.is_standing = True
-        time.sleep (1.5)
+        time.sleep (2.5)
+        #self.sport_client.Move(0.3,0,0)
+
 
     
     def stand_down(self):
-        self.StopMove()
-        self.Euler(0.0, 0.0, 0.0)
+        self.pressed_keys.clear()
+        self.sport_client.StopMove()
+        self.sport_client.Euler(0.0, 0.0, 0.0)
         time.sleep(0.2)
-        self.StandDown()
+        self.sport_client.StandDown()
         self.is_standing = False
-        time.sleep (1.5)
+        time.sleep(1.5)
         
 
     def run(self, stdscr):
@@ -56,6 +67,7 @@ class Go2KeyboardController(SportClient, ObstaclesAvoidClient):
         stdscr.refresh()
         self.stand_up()
         self.enable_obstacle_avoidance()
+        self.sport_client.ClassicWalk(True)
         
         try:
             while True:
@@ -70,7 +82,10 @@ class Go2KeyboardController(SportClient, ObstaclesAvoidClient):
                         elif key == ord('o'):
                             if self.obstacle_avoidance_enabled: self.disable_obstacle_avoidance()
                             else: self.enable_obstacle_avoidance()
+                        elif key == ord('k'):
+                            self.wave()
                         elif key in (ord('w'), ord('s'), ord('a'), ord('d'), ord('q'), ord('e'), 
+                        
                                         curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT):
                             if key in self.pressed_keys:
                                 self.pressed_keys.remove(key)
@@ -83,27 +98,56 @@ class Go2KeyboardController(SportClient, ObstaclesAvoidClient):
                 vx, vy, vyaw = 0.0, 0.0, 0.0
                 roll, pitch, yaw = 0.0, 0.0, 0.0
                 
-                if ord('w') in self.pressed_keys: vx += 0.5
-                if ord('s') in self.pressed_keys: vx -= 0.5
-                if ord('q') in self.pressed_keys: vy += 0.3
-                if ord('e') in self.pressed_keys: vy -= 0.3
-                if ord('a') in self.pressed_keys: vyaw += 0.5
-                if ord('d') in self.pressed_keys: vyaw -= 0.5
+                if ord('w') in self.pressed_keys:
+                    if ord('s') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('s'))
+                    vx += 0.5
                 
-                if curses.KEY_UP in self.pressed_keys: pitch += 0.2
-                if curses.KEY_DOWN in self.pressed_keys: pitch -= 0.2
-                if curses.KEY_LEFT in self.pressed_keys: yaw += 0.3
-                if curses.KEY_RIGHT in self.pressed_keys: yaw -= 0.3
+                if ord('s') in self.pressed_keys:
+                    if ord('w') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('w'))
+                    vx -= 0.5
+                    
+                if ord('q') in self.pressed_keys:
+                    if ord('e') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('e'))
+                    vy += 0.3
+                    
+                if ord('e') in self.pressed_keys:
+                    if ord('q') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('q'))
+                    vy -= 0.3
+                    
+                if ord('a') in self.pressed_keys:
+                    if ord('d') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('d'))
+                    vyaw += 0.5
+                    
+                if ord('d') in self.pressed_keys:
+                    if ord('a') in self.pressed_keys:
+                        self.pressed_keys.remove(ord('a'))
+                    vyaw -= 0.5
+                
+                if curses.KEY_UP in self.pressed_keys:
+                    pitch += 0.2
+                if curses.KEY_DOWN in self.pressed_keys:
+                    pitch -= 0.2
+                if curses.KEY_LEFT in self.pressed_keys:
+                    yaw += 0.3
+                if curses.KEY_RIGHT in self.pressed_keys:
+                    yaw -= 0.3
                 
                 #Send to Robot
+                
+
                 if self.is_standing:
-                    # Use ObstaclesAvoidClient.Move() when obstacle avoidance is enabled
+                    #Use ObstaclesAvoidClient.Move() when obstacle avoidance is enabled
                     if self.obstacle_avoidance_enabled:
-                        ObstaclesAvoidClient.Move(self, vx, vy, vyaw)  #ObstaclesAvoidClient.Move()
+                        self.obstacle_client.Move(vx, vy, vyaw)
                     else:
-                        SportClient.Move(self, vx, vy, vyaw)  #Explicitly call SportClient.Move()
-                    
-                    self.Euler(roll, pitch, yaw)
+                        self.sport_client.Move(vx, vy, vyaw)
+                        
+                    #self.sport_client.Euler(roll, pitch, yaw)
 
                 
                 # 4. Update SSH UI
@@ -133,13 +177,20 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python3 go2_ssh_controller.py <network_interface>")
         sys.exit(1)
-        
+
+
     network_interface = sys.argv[1]
-    ChannelFactoryInitialize(0, network_interface)
+    print(f"Network interface: {network_interface}")  # Add this
+    print(f"Args count: {len(sys.argv)}")
+
+    if len(sys.argv)>1:
+        ChannelFactoryInitialize(0, sys.argv[1])
+    else:
+        ChannelFactoryInitialize(0)
     controller = Go2KeyboardController(network_interface)
     curses.wrapper(controller.run)
 
 if __name__ == "__main__":
     main()
 
-#run command |python3 go2_keyboard_controller.py eth0
+#run command |python3 Go2KeyboardController.py eth0
